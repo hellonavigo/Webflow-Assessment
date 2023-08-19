556a874d48b3f7565fa536862f29de5337e01c12
Nicholas <Nicholas@hellonavigo.com>
2023-08-19 13:00:04 -0500

    Changed Code

--- custom-if-and-visiblity-statements/show-hide-alert.js
+++ custom-if-and-visiblity-statements/show-hide-alert.js
@@ -1,36 +1,16 @@
-// Get references to the elements
-const situationalAvgElement = document.getElementById('situational-avg');
-const adaptabilityAvgElement = document.getElementById('adaptability-avg');
-const efficacyAvgElement = document.getElementById('efficacy-avg');
-const noResultsAlertElement = document.getElementById('no-results-alert');
+document.addEventListener('DOMContentLoaded', function() {
+  // Get references to the elements
+  var adaptabilityParent = document.getElementById('adaptability-parent');
+  var noResultsAlert = document.getElementById('no-results-alert');
 
-// Check the values and update visibility
-function updateVisibility() {
-  const situationalAvgValue = parseFloat(situationalAvgElement.value);
-  const adaptabilityAvgValue = parseFloat(adaptabilityAvgElement.value);
-  const efficacyAvgValue = parseFloat(efficacyAvgElement.value);
-
-  console.log("Situational Avg:", situationalAvgValue);
-  console.log("Adaptability Avg:", adaptabilityAvgValue);
-  console.log("Efficacy Avg:", efficacyAvgValue);
-
-  if (
-    isNaN(situationalAvgValue) || situationalAvgValue === 0.0 || situationalAvgElement.value === '' ||
-    isNaN(adaptabilityAvgValue) || adaptabilityAvgValue === 0.0 || adaptabilityAvgElement.value === '' ||
-    isNaN(efficacyAvgValue) || efficacyAvgValue === 0.0 || efficacyAvgElement.value === ''
-  ) {
-    console.log("Showing no-results-alert");
-    noResultsAlertElement.style.display = 'block';
+  // Check if adaptabilityParent is hidden
+  if (adaptabilityParent.style.display === 'none') {
+    // Show noResultsAlert
+    noResultsAlert.style.display = 'block';
+    console.log('Showing no-results-alert');
   } else {
-    console.log("Hiding no-results-alert");
-    noResultsAlertElement.style.display = 'none';
+    // Hide noResultsAlert
+    noResultsAlert.style.display = 'none';
+    console.log('Hiding no-results-alert');
   }
-}
-
-// Attach the updateVisibility function to the input events of the elements
-situationalAvgElement.addEventListener('input', updateVisibility);
-adaptabilityAvgElement.addEventListener('input', updateVisibility);
-efficacyAvgElement.addEventListener('input', updateVisibility);
-
-// Initial visibility check
-updateVisibility();
+});
