cba4070d250cedebbd297f427c60c81d7aca0969
Nicholas <Nicholas@hellonavigo.com>
2023-08-24 14:56:54 -0400

    Show/hide

--- custom-if-and-visiblity-statements/show-hide-alert.js
+++ custom-if-and-visiblity-statements/show-hide-alert.js
@@ -19,10 +19,13 @@ document.addEventListener("DOMContentLoaded", function() {
   var adaptabilityParent = document.getElementById("adaptability-parent");
   var situationalAwarenessParent = document.getElementById("situational-awareness-parent");
   var selfEfficacyParent = document.getElementById("self-efficacy-parent");
+  var confidenceParent = document.getElementById("confidence-parent");
+  var creativityParent = document.getElementById("creativity-parent");
+  
   var noResultsAlert = document.getElementById("no-results-alert");
 
   // Check if both adaptabilityParent and situationalAwarenessParent are hidden
-  if (isHidden(adaptabilityParent) && isHidden(situationalAwarenessParent) && isHidden(selfEfficacyParent)) {
+  if (isHidden(adaptabilityParent) && isHidden(situationalAwarenessParent) && isHidden(selfEfficacyParent) && isHidden(confidenceParent) && isHidden(creativityParent)) {
     showElement(noResultsAlert);
     console.log("Showing no-results-alert");
   } else {
