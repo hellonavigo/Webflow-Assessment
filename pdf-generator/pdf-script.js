525203f53c06c26bab6c35cf02f43116cfb833eb
Nicholas <Nicholas@hellonavigo.com>
2023-08-28 16:49:31 -0400

    Updates

---
+++ pdf-generator/pdf-script.js
@@ -0,0 +1,19 @@
+document.querySelector('#download-pdf').addEventListener('click', function () {
+  const downloadButton = document.querySelector('#download-pdf');
+  const originalButtonText = downloadButton.textContent;
+  downloadButton.textContent = 'Generating...';
+
+  const contentToCapture = document.querySelector('.content-download');
+
+  html2canvas(contentToCapture).then((canvas) => {
+    const base64image = canvas.toDataURL('image/png');
+    const pdf = new jsPDF('p', 'px', [canvas.width, canvas.height]);
+    pdf.addImage(base64image, 'PNG', 0, 0, canvas.width, canvas.height);
+    pdf.save('attributes-results.pdf');
+
+    downloadButton.textContent = originalButtonText;
+  }).catch((error) => {
+    console.error('An error occurred:', error);
+    downloadButton.textContent = originalButtonText;
+  });
+});
