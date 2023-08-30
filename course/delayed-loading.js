bfd214196c13ab3fa524af86c71df641f78864a9
Nicholas <Nicholas@hellonavigo.com>
2023-08-30 18:48:02 -0400

    Delayed Loading

---
+++ course/delayed-loading.js
@@ -0,0 +1,30 @@
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const elementsToDelay = document.querySelectorAll('[ms-code-delay]');
+
+    elementsToDelay.forEach(element => {
+      element.style.visibility = "hidden"; // Hide the element initially
+    });
+
+    setTimeout(function() {
+      elementsToDelay.forEach(element => {
+        element.style.visibility = "visible"; // Make the element visible after the delay
+        element.style.opacity = "0"; // Set the initial opacity to 0
+        element.style.animation = "fadeIn 0.5s"; // Apply the fadeIn animation
+        element.addEventListener("animationend", function() {
+          element.style.opacity = "1"; // Set opacity to 1 at the end of the animation
+        });
+      });
+    }, 750);
+  });
+
+<style>
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+</style>
